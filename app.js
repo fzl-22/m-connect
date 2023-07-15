d0c2e5f1febfc7ddf7c1f656e237ca1e6e29113a
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-07-15 21:51:45 +0700

    added post

--- app.js
+++ app.js
@@ -1,23 +1,89 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const uuid = require("uuid");
+const express = require("express");
+const Intl = require("intl");
+const mysql = require("mysql");
+require("dotenv").config();
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
-
-app.get('/', function(req, res){
-  const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-  res.sendFile(htmlFilePath);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-app.get('/about', function(req, res){
-  const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-  res.sendFile(htmlFilePath);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
-app.get('/submission-form', function(req, res){
-  const htmlFilePath = path.join(__dirname, 'views', 'submission-form.html');
-  res.sendFile(htmlFilePath);
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
-app.listen(3000);
+app.get("/submission-form", function (req, res) {
+  res.render("submission-form");
+});
+
+app.post("/submission-form", function (req, res) {
+  const id = uuid.v4();
+  const now = new Date();
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  var createdAt = formatter.format(now).replace(/,/g, "");
+  createdAt = createdAt.toString().replace(/\//g, '-');
+  const editedAt = createdAt;
+
+  const requestBody = req.body;
+
+  const submission = {
+    id: id,
+    ...requestBody,
+    created_at: createdAt,
+    edited_at: editedAt,
+  };
+
+  const insertQuery = `INSERT INTO data_pengajuan (id, nama, nama_PT, alamat_PT, email, no_telp_wa, jenis_pengajuan, deskripsi, status, tanggal_dibuat, tanggal_diedit) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  const values = [
+    submission.id,
+    submission.nama,
+    submission.nama_PT,
+    submission.alamat_PT,
+    submission.email,
+    submission.no_telp_wa,
+    submission.jenis_pengajuan,
+    submission.deskripsi,
+    submission.status,
+    submission.created_at,
+    submission.edited_at,
+  ];
+
+  connection.query(insertQuery, values, (error, results) => {
+    if (error) {
+      console.error("Error inserting data:", error);
+      res.status(500).send("Error inserting data");
+    } else {
+      console.log("Data inserted successfully");
+      res.status(200).send("Data inserted successfully");
+    }
+  });
+
+  res.redirect("submission-form-success");
+});
+
+app.listen(3001);
